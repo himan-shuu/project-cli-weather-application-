970dd9c779840ece8c9cc66a296e305e36e78cc0
himan-shuu <76879328+himan-shuu@users.noreply.github.com>
2025-01-30 19:05:31 +0530

    Add files via upload

---
+++ quickweather.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+import json
+
+# Get location from user input instead of sys.argv
+location = input("Enter city/state name: ") 
+# to store api key to secure it
+
+url = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=c6b8070940a2e233f1b58b7a52d32d4f" % location
+response = requests.get(url)
+response.raise_for_status()
+
+weatherData = json.loads(response.text)
+data = weatherData['weather']
+print('current weather in %s:' % (location))
+print(data[0]['main'], "-", data[0]['description'])
+
+temp = weatherData['main']
+print('temperature is:', temp['temp'] - 273.25, "C")
+print()
